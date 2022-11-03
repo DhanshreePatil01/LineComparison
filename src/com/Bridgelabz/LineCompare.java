f9ca01d7d4bcb3c27cf1579ffb1d07b44ba0b6da
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-04 00:02:47 +0530

    Added : UC2 Lines Equality is checked.

--- src/com/Bridgelabz/LineCompare.java
+++ src/com/Bridgelabz/LineCompare.java
@@ -19,5 +19,27 @@ public class LineCompare {
 	double length1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 	System.out.println("Length of Line1 is: "+length1);
 	
+	System.out.print("Enter the value of x3 and y3: ");
+	int x3=input.nextInt();
+	int y3=input.nextInt();
+	
+	System.out.print("Enter the value of x4 and y4: ");
+	int x4=input.nextInt();
+	int y4=input.nextInt();
+	
+	double length2=Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+	System.out.println("Length of Line2 is: "+length2);
+	
+	String line1=String.valueOf(length1);
+	String line2=String.valueOf(length2);
+	
+	if(line1.equals(line2))
+	{
+		System.out.println("Two Lines are equal");
+	}
+	else 
+	{
+		System.out.println("Two Lines are not equal");
+	}
   }
 }
